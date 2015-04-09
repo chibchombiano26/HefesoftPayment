97f5a5412736301b267c3b5fef481d0071082927
Jose douglas ramirez <chibchombiano26@hotmail.com>
2015-04-08 22:43:18 -0500

    publish

--- Hefesoft.Payment/obj/Release/Package/PackageTmp/server.js
+++ Hefesoft.Payment/obj/Release/Package/PackageTmp/server.js
@@ -3,13 +3,18 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var stripe = require('stripe')('sk_test_lf2op9O6fVZYvWs4v3rpjGQF');
 var path = require('path');
+var azure = require('azure-storage');
+var tableSvc = azure.createTableService('hefesoft', 'dodn17DT7hBi3lXrWlvXihLS9J7xuItHLIpWLBZn2QEMdBHm02Lqxr055rNCpP5z3FhfcjjX3MhPy1Npk3VF3Q==');
+
 
 var app = express();
 app.use(bodyParser());
 
 
 
-app.post('/charge', function(req, res) {
+app.post('/charge', function (req, res) {
+    tableServices('TpToken');    
+
     var stripeToken = req.body.stripeToken;
     var amount = 1000;
 
@@ -23,9 +28,37 @@ app.post('/charge', function(req, res) {
             res.send(500, err);
         } else {
             res.send(204);
+            insert('TpToken', null);
         }
     });
 });
+
+
+function tableServices(nombre){    
+    tableSvc.createTableIfNotExists(nombre, function (error, result, response) {
+        if (!error) {
+    // result contains true if created; false if already exists
+        }
+    });
+}
+
+function insert(table,entity){
+    var entGen = azure.TableUtilities.entityGenerator;
+    var task = {
+        PartitionKey: entGen.String('hometasks'),
+        RowKey: entGen.String('1'),
+        description: entGen.String('take out the trash'),
+        dueDate: entGen.DateTime(new Date(Date.UTC(2015, 6, 20))),
+    };
+    
+    entity = task;
+
+    tableSvc.insertEntity(table, entity, function (error, result, response) {
+        if (!error) {
+    // result contains the ETag for the new entity
+        }
+    });
+}
 
 var port = process.env.port || 1337;
 app.use(express.static(__dirname));
